ddae71d2238f3fffa25f27825c05f9f544c37914
Udalych <vovarx@gmail.com>
2018-05-24 18:11:30 +0300

    Add [templating]: add simple templating

--- src/app.php
+++ src/app.php
@@ -1,9 +1,44 @@
 <?php
 
 use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
+
+function render_template($request): Response
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
 
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/myPhpFramework/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Routing\Route('/myPhpFramework/bye'));
+$routes->add('hello', new Routing\Route('/myPhpFramework/hello/{name}', [
+    'name' => 'World',
+    '_controller' => function($request) {
+        return render_template($request);
+    }
+]));
+
+$routes->add('bye', new Routing\Route('/myPhpFramework/bye', ['_controller' => 'render_template']));
+
+$routes->add('leap_year', new Routing\Route('/myPhpFramework/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => function ($request) {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
+        return new Response('Nope, this is not a leap year!');
+    }
+]));
 
 return $routes;
